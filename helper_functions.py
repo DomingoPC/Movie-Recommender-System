7235eeb55d6b0e70348406db7911a0d6943b4a03
DomingoPC <parralesdelacruzdomingo@gmail.com>
2025-03-14 14:21:01 +0100

    TF-IDF for CBRS. Extra LSH to approximate similarity.

--- helper_functions.py
+++ helper_functions.py
@@ -64,7 +64,59 @@ def show_correlation(df):
     return corr
 
 # --- TF-IDF ---
+import pyspark.sql.functions as f
+from pyspark.sql.types import FloatType, StringType
+from pyspark.sql import DataFrame
 
+# Create similarity function with a target vector
+def create_similarity_function(spark, movie_id: int, method: str = 'cosine', results: DataFrame = None):
+    # Method validation
+    if method not in ('cosine', 'jaccard'):
+        print('Error: only "cosine" or "jaccard" methods available.')
+        return None
+    
+    # Movie selector validation
+    try:
+        # Get title and vector representation
+        movie = results.\
+            filter(results.id == movie_id).\
+            select('title', 'features').collect()[0]
+
+        # Show movie name
+        print(f'Chosen movie: {movie[0]}.')
+
+        # Broadcast (cache) movie vector for quick access
+        bc_mv = spark.sparkContext.broadcast(movie[1])
+
+    except IndexError:
+        print(f'Error: no movie with the index {movie_id} was found.')
+        return None
+
+    # Chose method to compute similarity
+    if method == 'cosine':
+        def sim_computation(other_movie):
+            # Formula: sim = A·B /|A||B|
+            num = float(np.dot(bc_mv.value, other_movie))
+            
+            mod_a = float(np.sqrt(np.dot(bc_mv.value, bc_mv.value)))
+            mod_b = float(np.sqrt(np.dot(other_movie, other_movie)))
+            den = mod_a * mod_b
+
+            # If either A or B is an empty string, there is
+            # nothing to compare, thus return no similarity (0)
+            return num / den if den != 0 else 0.0
+    
+    else: # jaccard similarity
+        def sim_computation(other_movie):
+            # Formula: Sim = intersection(A,B) / Union(A,B)
+            A_and_B = float(sum(1 for (a,b) in zip(bc_mv.value,other_movie) if a==1 and a==b)) # Intersection
+            A_or_B = float(sum(1 for (a,b) in zip(bc_mv.value,other_movie) if a==1 or b==1)) # Union
+            
+            # If either A or B is an empty string, there is
+            # nothing to compare, thus return no similarity (0)
+            return A_and_B / A_or_B if A_or_B != 0 else 0.0 # Similarity
+
+    return f.udf(lambda vec: sim_computation(vec), FloatType())
 
 
 
